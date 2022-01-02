84f01f0f2cd38e1ba08d651fef930d4b52036653
Jasper Dirix <74553423+JasperDirixPXL@users.noreply.github.com>
2022-01-02 09:22:42 +0100

    Update pm2.config.js

--- pm2.config.js
+++ pm2.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name      : 'calcApp',
-    script    : 'server.js',
+    script    : '/etc/opt/calcApp/server.js',
     node_args : '-r dotenv/config'
   }],
 }
